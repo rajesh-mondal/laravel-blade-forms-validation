c2a11c6632fe5c4e49e66eba34b3d593e2507d53
Rajesh Mondal <rajesh01749@gmail.com>
2025-08-28 17:22:40 +0600

    implement validation rules for form fields

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller {
@@ -10,6 +11,14 @@ class ProductController extends Controller {
     }
 
     function store( Request $request ) {
+        $validated = $request->validate( [
+            'name'        => 'required|string|min:5|max:50',
+            'price'       => 'required|numeric|min:3.0|max:100.0',
+            'description' => 'nullable|string',
+            'stock'       => 'required|boolean',
+        ] );
 
+        Product::create( $validated );
+        return redirect()->back();
     }
 }
