ca4ff14db6b48a7dc4cb210defb8e93b8bafcea5
Rajesh Mondal <rajesh01749@gmail.com>
2025-08-29 16:06:47 +0600

    move rules to custom request for cleaner code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -10,20 +11,26 @@ class ProductController extends Controller {
         return view( 'product.store' );
     }
 
-    function store( Request $request ) {
-        $validated = $request->validate( [
-            'name'        => 'required|string|min:5|max:50',
-            'price'       => 'required|numeric|min:3.0|max:100.0',
-            'description' => 'nullable|string',
-            'stock'       => 'required|boolean',
-        ], [
-            'name.min' => 'পন্যের নাম সর্বনিন্ম পাঁচ অক্ষরের হতে হবে',
-            'name.max' => 'পন্যের নাম সর্বোচ্চ পঞ্চাশ অক্ষরের হতে হবে',
-            'name.required' => 'পন্যের মূল্য দিতেই হবে',
-            'price.required' => 'মূল্য দিতেই হবে',
-            'price.min' => 'একটা পন্যের সর্বনিন্ম মূল্য ৩ টাকা হতে হবে'
-        ] );
+    // function store( Request $request ) {
+    //     $validated = $request->validate( [
+    //         'name'        => 'required|string|min:5|max:50',
+    //         'price'       => 'required|numeric|min:3.0|max:100.0',
+    //         'description' => 'nullable|string',
+    //         'stock'       => 'required|boolean',
+    //     ], [
+    //         'name.min' => 'পন্যের নাম সর্বনিন্ম পাঁচ অক্ষরের হতে হবে',
+    //         'name.max' => 'পন্যের নাম সর্বোচ্চ পঞ্চাশ অক্ষরের হতে হবে',
+    //         'name.required' => 'পন্যের মূল্য দিতেই হবে',
+    //         'price.required' => 'মূল্য দিতেই হবে',
+    //         'price.min' => 'একটা পন্যের সর্বনিন্ম মূল্য ৩ টাকা হতে হবে'
+    //     ] );
 
+    //     Product::create( $validated );
+    //     return redirect()->back()->with( 'success', 'Product created successfully' );
+    // }
+
+    function store( ProductRequest $request ) {
+        $validated = $request->validated();
         Product::create( $validated );
         return redirect()->back()->with( 'success', 'Product created successfully' );
     }
---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest 
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool 
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array 
+    {
+        return [
+            'name'        => 'required|string|min:5|max:50',
+            'price'       => 'required|numeric|min:3.0|max:100.0',
+            'description' => 'nullable|string',
+            'stock'       => 'required|boolean',
+        ];
+    }
+
+    public function messages(): array 
+    {
+        return [
+            'name.min'       => 'পন্যের নাম সর্বনিন্ম পাঁচ অক্ষরের হতে হবে',
+            'name.max'       => 'পন্যের নাম সর্বোচ্চ পঞ্চাশ অক্ষরের হতে হবে',
+            'name.required'  => 'পন্যের মূল্য দিতেই হবে',
+            'price.required' => 'মূল্য দিতেই হবে',
+            'price.min'      => 'একটা পন্যের সর্বনিন্ম মূল্য ৩ টাকা হতে হবে',
+        ];
+    }
+}
